c7a17d628be95bb0f501a27023e7be85d20f000e
Darryl Eason <darryleason4@gmail.com>
2025-09-30 09:52:24 -0500

    Added audio management ('not complete')

--- include/audio_manager.hpp
+++ include/audio_manager.hpp
@@ -1,32 +1,39 @@
-#ifndef AUDIO_MANAGER_HPP
-#define AUDIO_MANAGER_HPP
+// #ifndef AUDIO_MANAGER_HPP
+// #define AUDIO_MANAGER_HPP
 
-#include <SFML/Audio.hpp>
-#include <string>
-#include <map>
+// #include <SFML/Audio.hpp>
+// #include <string>
+// #include <map>
+// #include <memory>
 
-class AudioManager
-{
-private:
+// class AudioManager
+// {
+// private:
 
-    std::map<std::string, sf::Sound> Sounds;
-    std::map<std::string, sf::Music> Tracks;
+//     std::map<std::string, sf::Sound> Sounds;
+//     std::map<std::string, std::unique_ptr<sf::Music>> Songs;
 
-public:
+// public:
 
-void loadSound();
-void loadSong();
-void changeSong();
-
-
-
-
+// void loadSound(const std::string filename, const std::string effectname);
+// void playSong(const std::string filename, const std::string title);
+// void changeSong(const std::string title, const std::string filename);
+// void loopSong();
 
 
-};
+// sf::Sound& getSoundRef(const std::string& sound);
+// sf::Music& getMusicRef(const std::string& track);
+
+
+// AudioManager()
+// {
+// }
+
+
+// };
 
 
 
 
 
-#endif
+// #endif
